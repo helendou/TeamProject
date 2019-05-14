aa221fa0fbd4555511f3d31f7e1321addb874fc4
helendou <dhy2018@gmail.com>
2019-05-14 14:23:23 -0400

    little update

--- homepage.php
+++ homepage.php
@@ -8,22 +8,28 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <title></title>
   </head>
   <body>
-    <div class="container">
+    <div class="container" style= "background-color: #e3f2fd;>
     <nav class="navbar navbar-default">
         <div class="navbar-header">
           <img style="max-width:200px; padding:30px;"src="img/weblogo.jpg" widthalt="logo">
         </div>
           <ul class="nav navbar-nav navbar-right；pagination">
-            <li class="page-item"><a class="page-link" href="welcome.php">Welcome</a></li>
+            <li class="page-item"><a class="page-link" href="welcome.php">About Us</a></li>
             <li class="page-item"><a class="page-link"href="order.html">Order</a></li>
             <li class="page-item"><a class="page-link"href="information.html">Information</a></li>
             <li class="page-item"><a class="page-link"href="contact.html">Contact</a></li>
          </ul>
+      <form class="pull-right">
          <a href="registration_log-in_php/login.php"><button type="button" class="btn btn-success">LOG IN</button></a>
          <a href="registration_log-in_php/register.php"><button type="button" class="btn btn-success">SIGN UP</button></a>
+      </form>
     </nav>
   </div>
 
@@ -31,7 +37,7 @@
        <div class="container">
          <div class="row">
         <div class="col-sm-4">
-          <a href="information.html"> Welcome information
+          <a href="information.html"> About Us
           <img src="img/service.jpg" alt="info" class="img-responsive" width="304" height="236">
           <button class="btn btn-success">Click me</button>
           </a>
@@ -53,18 +59,27 @@
        </div>
     </div>
 
-    <div class="container" style= "padding-top: 60px;"">
-    <footer>
-            <ul>
-                <li><a href="welcome.html">Welcome</a></li>
-                <li><a href="information.html">Information</a></li>
-                <li><a href="order.html">Order</a></li>
-                <li><a href="contact.html">Contact</a></li>
-            </ul>
-    </footer>
-  </div>
+    <hr>
+    <div class="container" style="background-color: #e3f2fd;">
+                <div class="text-center center-block">
+                  <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+    	            <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+    	            <a href="https://plus.google.com/+Bootsnipp-page"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+    	            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+               </div>
+        <div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+					<p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
+					<p class="h6">&copy All right Reversed</p>
+				</div>
+				</hr>
+			</div>
+    </div>
+
+
+
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-</body>
  </body>
   </html>
